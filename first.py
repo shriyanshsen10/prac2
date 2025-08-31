8b352a1a6514c5360a48501d093a342c0ef9917a
shriyanshsen10 <shriyanshsen10@gmail.com>
2025-08-31 18:09:41 +0530

    5 commit according to him

--- first.py
+++ first.py
@@ -1,3 +1,4 @@
 print("hey hey hey")
 print("hello hello hello")
 print("I made a brach. Yahoo!")
+str2 = "hey new one branch coming to master the damn topics. Todya is the finally the day i rise upin myself bingo!"
